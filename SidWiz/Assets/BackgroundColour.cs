f0685151aa97583e18db8241736953e134965c13
greigs <>
2021-06-09 00:59:34 +0100

    Beat causes platform to appear, Colour background changes on beat

---
+++ SidWiz/Assets/BackgroundColour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundColour : MonoBehaviour
+{
+    private bool change;
+
+    void OnEnable()
+    {
+        AudioTimeline.Beat += OnBeatHandler;
+    }
+
+    void OnDisable()
+    {
+        AudioTimeline.Beat -= OnBeatHandler;
+    }
+
+    private void OnBeatHandler()
+    {
+        change = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (change)
+        {
+            var renderer = GetComponent<Renderer>();
+            var color = Random.ColorHSV(0f, 1f, 1, 1,1,1);
+            renderer.material.SetColor("_ReflectColor", color);
+            //renderer.material.SetColor("_Main Color", color);
+            renderer.material.color = color;
+            change = false;
+        }
+
+        //renderer.material.SetColor(1, color);
+    }
+}
--- SidWiz/Assets/PlatformCreator.cs
+++ SidWiz/Assets/PlatformCreator.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlatformCreator : MonoBehaviour
 {
@@ -8,6 +10,7 @@ public class PlatformCreator : MonoBehaviour
     public GameObject myPrefab;
     public GameObject playerObject;
     public int xOffset = 10;
+    private int countDownToGap = 4;
 
     void OnEnable()
     {
@@ -21,6 +24,16 @@ public class PlatformCreator : MonoBehaviour
 
     void OnBeatHandler()
     {
-        Instantiate(myPrefab, new Vector3(playerObject.transform.position.x + xOffset, 0, -5), Quaternion.identity);
+        countDownToGap--;
+        if (countDownToGap == 0)
+        {
+            countDownToGap = Random.Range(4, 8);
+            Instantiate(myPrefab, new Vector3(playerObject.transform.position.x + xOffset, 4, -5), Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(myPrefab, new Vector3(playerObject.transform.position.x + xOffset, 0, -5), Quaternion.identity);
+        }
+        
     }
 }
